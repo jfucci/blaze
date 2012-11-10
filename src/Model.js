90e4a910e3c474c4133b0168daa72cb06a67f714
jfucci <jacob_fucci@buacademy.org>
2012-11-10 18:23:22 -0500

    replaced forestArray array with a forestArray object; replaced for loops with _.each; replaced isBurning function with an isBurning boolean variable; added copterSquare; added neighbors array to blaze.Square

--- src/Model.js
+++ src/Model.js
@@ -3,76 +3,75 @@
 	"use strict";
 
 	blaze.Model = function(setup) {
-		this.getGridSize = _.constant(setup.gridSize);
-		this.getPercentGreen = _.constant(setup.percentGreen);
+		this.getGridSize      = _.constant(setup.gridSize);
+		this.getPercentGreen  = _.constant(setup.percentGreen);
 		this.getWaterTankSize = _.constant(setup.waterTankSize);
-		this.getFlammability = _.constant(setup.flammability);
-		this.getBurnRate = _.constant(setup.burnRate);
-		this.trees = 0;
+		this.getFlammability  = _.constant(setup.flammability);
+		this.getBurnRate      = _.constant(setup.burnRate);
+		this.trees            = 0;
+		this.isBurning        = false;
+		var coordinates = _.product(_.repeat(_.range(this.getGridSize()), 2));
+		this.forestArray = {};
+		_.each(coordinates, function(coordinate) {
+			this.forestArray[coordinate] = new blaze.Square(coordinate[0], coordinate[1]);
+		}, this);
+
 	};
 
 	blaze.Model.prototype.restart = function() {
+		this.copterSquare = [];
+		this.isBurning = true;
 		this.waterLevel = 100;
 		this.trees = 0;
-		this.forestArray = [];
-		for(var x = 0; x < this.getGridSize(); x++) {
-			this.forestArray[x] = [];
-			for(var y = 0; y <= this.getGridSize(); y++) {
-				this.forestArray[x][y] = new blaze.Square(x, y);
-				if(Math.random() > this.getPercentGreen()) {
+		_.each(this.forestArray, function(square) {
+			square.setup(this.getGridSize());
+			if(Math.random() > this.getPercentGreen()) {
 					this.trees++;
-					if(x === 0) {
-						this.forestArray[x][y].percentBurned += this.getBurnRate();
+					if(square.getX() === 0) {
+						square.percentBurned += this.getBurnRate();
 					} else {
-						this.forestArray[x][y].flammable = true;
+						square.flammable = true;
 					}
 				}
-			}
-		}
-	};
-
-	blaze.Model.prototype.isBurning = function() {
-		for(var x = 0; x < this.getGridSize(); x++) {
-			for(var y = 0; y < this.getGridSize(); y++) {
-				if(this.forestArray[x][y].percentBurned > 0 && this.forestArray[x][y].percentBurned < 1) {
-					return true;
-				}
-			}
-		}
-		return false;
+		}, this);
 	};
 
 	blaze.Model.prototype.step = function() {
-		for(var x = 0; x < this.getGridSize(); x++) {
-			for(var y = 0; y < this.getGridSize(); y++) {
-				if(this.forestArray[x][y].percentBurned > 1) {
-					this.forestArray[x][y].percentBurned = 1;
-				} else if(this.forestArray[x][y].percentBurned > 0 && this.forestArray[x][y].percentBurned < 1) {
-					this.forestArray[x][y].percentBurned += this.getBurnRate();
-					this.burn(x, y);
-				}
+		_.each(this.forestArray, function(square) {
+			if(square.percentBurned > 1) {
+				square.percentBurned = 1;
+			} else if(square.percentBurned > 0 && square.percentBurned < 1) {
+				square.percentBurned += this.getBurnRate();
+				this.burn(square.getX(), square.getY());
 			}
-		}
+		}, this);
 	};
 
 	blaze.Model.prototype.burn = function(x, y) {
-		for(var xx = x - 1; xx <= x + 1; xx++) {
-			for(var yy = y - 1; yy <= y + 1; yy++) {
-				if(this.forestArray[xx] && this.forestArray[xx][yy]) {
-					if(this.forestArray[xx][yy].flammable && Math.random() < this.getFlammability()) {
-						this.forestArray[xx][yy].percentBurned += this.getBurnRate();
-						this.forestArray[xx][yy].flammable = false;
-					}
-				}
+		_.each(this.forestArray[[x, y]].neighbors, function(neighbor) {
+			if(this.forestArray[neighbor].flammable && Math.random() < this.getFlammability()) {
+				this.forestArray[neighbor].percentBurned += this.getBurnRate();
+				this.forestArray[neighbor].flammable = false;
 			}
-		}
+		}, this);
 	};
 
 	blaze.Square = function(x, y) {
 		this.getX = _.constant(x);
 		this.getY = _.constant(y);
-		this.flammable = false;
+	};
+
+	blaze.Square.prototype.setup = function(gridSize) {
+		this.flammable     = false;
 		this.percentBurned = 0;
-		this.watered = false;
+		this.watered       = false;
+		this.neighbors = [];
+		for(var xx = this.getX() - 1; xx <= this.getX() + 1; xx++) {
+			for(var yy = this.getY() - 1; yy <= this.getY() + 1; yy++) {
+				if(xx >= 0 && yy >= 0 && xx < gridSize && yy < gridSize) {
+					this.neighbors.push([xx, yy]);
+				}
+			}
+		}
 	};
 }());
